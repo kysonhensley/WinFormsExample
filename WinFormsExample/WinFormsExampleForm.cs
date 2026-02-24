951b91e74dd61600d782235b1fd12697c2d2c4da
kysonhensley <hensleykyson@gmail.com>
2026-02-24 08:44:49 -0700

    add set defaults method

--- WinFormsExample/WinFormsExampleForm.cs
+++ WinFormsExample/WinFormsExampleForm.cs
@@ -5,7 +5,23 @@ namespace WinFormsExample
         public WinFormsExampleForm()
         {
             InitializeComponent();
+            SetDefaults();
         }
+
+        private void SetDefaults()
+        {
+            NameTextBox.Text = "";
+            AgeTextBox.Text = "";
+            CityTextBox.Text = "";
+            PhoneTextBox.Text = "";
+
+            UpperCaseRadioButton.Checked = true;
+            LowerCaseRadioButton.Checked = false;
+            ReverseButton.Checked = false;
+        }
+
+        //Event Handlers Below
+
         private void ExitButton_Click(object sender, EventArgs e)
         {
             this.Close();
@@ -14,6 +30,11 @@ namespace WinFormsExample
         private void SubmitButton_Click(object sender, EventArgs e)
         {
             this.Text = NameTextBox.Text;
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            SetDefaults();
         }
     }
 }
